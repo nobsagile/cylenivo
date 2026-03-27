7b991066def6985e87daed6ca80e6945a2ac22c4
Thomas NBA <nobsagile@gmail.com>
2026-03-27 13:45:45 +0100

    v0.8.1: remove legacy Python backend, frontend dir, connectors

--- backend/app/importers/__init__.py
+++
@@ -1,7 +0,0 @@
-from app.importers.jira_importer import JiraImporter
-
-IMPORTER_REGISTRY: dict[str, type] = {
-    "jira": JiraImporter,
-}
-
-__all__ = ["IMPORTER_REGISTRY", "JiraImporter"]
--- backend/app/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from app.models.project_config import ProjectConfig
-from app.models.import_session import ImportSession
-from app.models.ticket import Ticket
-from app.models.ticket_transition import TicketTransition
-from app.models.llm_insight import LLMInsight
-
-__all__ = ["ProjectConfig", "ImportSession", "Ticket", "TicketTransition", "LLMInsight"]
