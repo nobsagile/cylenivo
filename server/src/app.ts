576110af3e89ab55318ee1266b40a59a0688ae2e
Thomas NBA <nobsagile@gmail.com>
2026-04-04 14:11:09 +0200

    v0.57.33: add request logging in dev mode

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,6 @@
 import { Hono } from 'hono'
 import { cors } from 'hono/cors'
+import { logger } from 'hono/logger'
 import configs from './routes/configs.js'
 import imports from './routes/imports.js'
 import metrics from './routes/metrics.js'
@@ -12,6 +13,7 @@ import demo from './routes/demo.js'
 export const app = new Hono()
 
 app.use('*', cors())
+if (process.env.NODE_ENV === 'development') app.use('*', logger())
 
 app.get('/health', (c) => c.json({ status: 'ok' }))
 
