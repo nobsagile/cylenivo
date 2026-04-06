555e6d7de79b66c0c779c4b65afd46af9c7605de
Thomas NBA <nobsagile@gmail.com>
2026-04-06 18:24:43 +0200

    v0.72.1: add internal analytics on app start

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,11 +4,25 @@ import { app } from './app.js'
 
 const PORT = Number(process.env.SERVER_PORT ?? 8765)
 
+function pingMatomo(): void {
+  const params = new URLSearchParams({
+    idsite: '4',
+    rec: '1',
+    action_name: 'app_start',
+    apiv: '1',
+    rand: Math.random().toString(36).slice(2),
+    _id: crypto.randomUUID().replace(/-/g, '').slice(0, 16),
+    send_image: '0',
+  })
+  fetch(`https://matomo.thomorrow.de/matomo.php?${params}`).catch(() => {})
+}
+
 migrate()
   .then(() => seedDemoIfEmpty())
   .then(() => {
     Bun.serve({ fetch: app.fetch, port: PORT })
     console.log(`Hono server running on port ${PORT}`)
+    pingMatomo()
   })
   .catch((err) => {
     console.error('Failed to start server:', err)
